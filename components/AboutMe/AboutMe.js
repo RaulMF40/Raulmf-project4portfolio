907043025e6690dde69970fc050e519d06af45af
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-06-25 14:25:08 +0200

    Update AboutMe.js

--- components/AboutMe/AboutMe.js
+++ components/AboutMe/AboutMe.js
@@ -4,7 +4,7 @@ const aboutMeInfo = {
   name: 'Raúl Moya Fuentes',
   address: 'Málaga, Spain',
   email: 'raulmf.30@gmail.com',
-  avatar: './assets/yo.png',
+  avatar: './assets/yo.jpg',
   aboutMe:
     'With a solid background in administration and current experience in the comic book and merchandising world, my curiosity for digital technology led me to embark on an exciting professional transition. At the end of 2023, I began my training as a full stack developer at The Power MBA, driven by my interest in web development and emerging technologies such as blockchain. This journey has been a thrilling challenge, full of ups and downs, but each new discovery in the field of digital technology and learning concepts previously unknown to me has reinforced my decision to professionally reinvent myself in this dynamic sector. I am currently expanding my knowledge and skills in the development of complete web applications, covering both front-end and back-end. My training at The Power MBA is equipping me with the necessary tools to create innovative and efficient technological solutions. I am familiar with a variety of technologies and programming languages, including HTML, CSS, JavaScript, React, Angular, Python, etc.., and I continue to explore other technologies and frameworks that complement my profile as a full stack developer.'
 }
